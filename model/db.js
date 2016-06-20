614435a6fae8c256fe542c37eccdd6afc97bede8
Mayszak <mayszaks@f45c898a283d.ant.amazon.com>
2016-06-20 11:18:42 -0700

    mongoose changes to create schema on start

--- model/db.js
+++ model/db.js
@@ -14,45 +14,10 @@ var options = {
 var mongoose = require( 'mongoose' );
 mongoose.connect(dbURI, options);
 
-var categorySchema = new mongoose.Schema({
-    name: String
-});
-mongoose.model('category', categorySchema, 'categories');
-var memberSchema = new mongoose.Schema({
-    name: String,
-    created: Date,
-    defaultCategory: [categorySchema],
-    categories: [categorySchema]
-});
-mongoose.model('member', memberSchema);
-var accountSchema = new mongoose.Schema({
-    members: [memberSchema],
-    login: String,
-    password: String
-})
-mongoose.model('account', accountSchema);
-
-var transactionSchema =  new mongoose.Schema({
-    categoryId: mongoose.Schema.Types.ObjectId,
-    memberId: mongoose.Schema.Types.ObjectId,
-    accountId: mongoose.Schema.Types.ObjectId,
-    category: String,
-    member: String,
-    cost: Number,
-    date: Date,
-    comments: String,
-    payment: String
-});
-mongoose.model('transaction', transactionSchema);
-
 mongoose.connection.on('error',function (err) {
     console.log('Mongoose connection error: ' + err);
 });
 
-
-mongoose.connection.on('error',function (err) {
-    console.log('Mongoose connection error: ' + err);
-});
 mongoose.connection.on('disconnected', function () {
     console.log('Mongoose disconnected');
 });
@@ -62,4 +27,37 @@ process.on('SIGINT', function() {
         console.log('Mongoose disconnected through app termination');
         process.exit(0);
     });
+});
+
+mongoose.connection.on('open', function(){
+        var categorySchema = new mongoose.Schema({
+        name: String
+    });
+    mongoose.model('category', categorySchema, 'categories');
+    var memberSchema = new mongoose.Schema({
+     name: String,
+     created: Date,
+     defaultCategory: [categorySchema],
+     categories: [categorySchema]
+    });
+    mongoose.model('member', memberSchema);
+    var accountSchema = new mongoose.Schema({
+     members: [memberSchema],
+     login: String,
+     password: String
+    })
+    mongoose.model('account', accountSchema);
+
+    var transactionSchema =  new mongoose.Schema({
+    categoryId: mongoose.Schema.Types.ObjectId,
+    memberId: mongoose.Schema.Types.ObjectId,
+    accountId: mongoose.Schema.Types.ObjectId,
+    category: String,
+    member: String,
+    cost: Number,
+    date: Date,
+    comments: String,
+    payment: String
+    });
+    mongoose.model('transaction', transactionSchema);
 });
